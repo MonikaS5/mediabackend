dd44b777aba50d46ee6c14eda3a0df5dd86fc29d
MonikaS5 <monika.bhagade@gmail.com>
2024-11-01 22:02:58 +0000

    server setup2

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
+//load env variable
 dotenv.config();
+
+//set port
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+//Database connection
+connectDB();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+//error handling middleware
+app.use((error, req, res, next) => {
+    console.error(error)
+    res.status(500).json({ message: 'Internal Server Error', error: error.message });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
